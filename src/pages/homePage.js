c109835bc6558685dd41a6935fc7f9694da35421
Jake Fletcher <20080415@mail.wit.ie>
2020-11-11 15:56:26 +0000

    Step 4 finished
    Still have a compile error from step 2.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect } from "react";
-
+import StubAPI from "../api/stubAPI";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
@@ -21,7 +21,7 @@ const MovieListPage = () => {
         setMovies(movies);
       });
   }, []);
-  
+
   const genre = Number(genreFilter)
   let displayedMovies = movies
     .filter(m => {
@@ -35,12 +35,25 @@ const MovieListPage = () => {
     if (type === "name") setTitleFilter(value);
     else setGenreFilter(value);
   };
+
+  const addToFavorites = movieId => {
+    // Find index position of selected movie in the list
+    const index = movies.map(m => m.id).indexOf(movieId)
+
+    StubAPI.add(movies[index])
+    const updatedList = [...movies]  // Make a copy of the movie list
+    updatedList.splice(index, 1)     // Remove selected movie from home page list
+    setMovies(updatedList)  
+  }
   
   return (
     <>
       <Header numMovies={displayedMovies.length} />
       <FilterControls onUserInput={handleFilterChange} />
-      <MovieList movies={displayedMovies} /> 
+      <MovieList
+        movies={displayedMovies}
+        buttonHandler={addToFavorites}
+      />
     </>
   );
 };
